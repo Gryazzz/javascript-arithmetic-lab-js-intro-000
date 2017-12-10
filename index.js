7bc07d71719123d0e31784b770ea67b39ffd3858
Sonik S <gryazzz@gmail.com>
2017-12-10 00:30:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,48 @@
+var a = 6
+var b = 3
+
+function add() {
+  return console.log(add(a, b));
+}
+
+function subtract() {
+  return subtract(a, b);
+}
+
+function multiply() {
+  return multiply(a, b);
+}
+
+function divide() {
+  return divide(a, b);
+}
+
+function inc() {
+  a++;
+  return console.log(a);
+}
+
+function dec() {
+  a--;
+  return console.log(a);
+}
+
+function makeInt() {
+  return parseInt(n);
+}
+
+function makeInt() {
+  return parseInt(n, 10);
+}
+
+function makeInt() {
+  return parseInt('n');
+}
+
+function preserveDecimal() {
+  return parseFloat(n)
+}
+
+function preserveDecimal() {
+  return parseFloat('n')
+}
